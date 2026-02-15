70c1e7fd3f6f62e9a3e2cfcbd8b561928d29f36a
Anirudh <shivammm.hyphen@gmail.com>
2026-02-15 20:47:26 +0530

    add: client streaming

--- src/main/java/org/anirudh/spring/tradingserver/service/StockTradingServiceImpl.java
+++ src/main/java/org/anirudh/spring/tradingserver/service/StockTradingServiceImpl.java
@@ -1,8 +1,6 @@
 package org.anirudh.spring.tradingserver.service;
 
-import com.anirudh.grpc.StockRequest;
-import com.anirudh.grpc.StockResponse;
-import com.anirudh.grpc.StockTradingServiceGrpc;
+import com.anirudh.grpc.*;
 import io.grpc.stub.StreamObserver;
 import org.anirudh.spring.tradingserver.entity.Stock;
 import org.anirudh.spring.tradingserver.repository.StockRepository;
@@ -60,5 +58,42 @@ public class StockTradingServiceImpl extends StockTradingServiceGrpc.StockTradin
 
         responseObserver.onCompleted();
 
+    }
+
+    @Override
+    public StreamObserver<StockOrder> bulkStockOrder(StreamObserver<OrderSummary> responseObserver) {
+        return new StreamObserver<StockOrder>() {
+
+            private int totalOrder = 0;
+            private double totalAmount = 0;
+            private int successCount = 0;
+
+            @Override
+            public void onNext(StockOrder stockOrder) {
+                totalOrder++;
+                totalAmount = totalAmount + (stockOrder.getQuantity() * stockOrder.getPrice());
+                successCount++;
+
+                System.out.println("Recieved order \n" + stockOrder);
+            }
+
+            @Override
+            public void onError(Throwable throwable) {
+                System.out.println(throwable.getMessage());
+            }
+
+            @Override
+            public void onCompleted() {
+
+                OrderSummary summary = OrderSummary.newBuilder()
+                        .setTotalOrder(totalOrder)
+                        .setTotalAmount(totalAmount)
+                        .setSuccessCount(successCount)
+                        .build();
+
+                responseObserver.onNext(summary);
+                responseObserver.onCompleted();
+            }
+        };
     }
 }
